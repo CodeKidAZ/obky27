2984bec120f107391a7f6bec6f2a0c02e0f690f2
CodeKidAZ <snair9@asu.edu>
2023-06-16 15:01:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# obky27
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/obky27)
---
+++ src/app/ad-banner.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+import { AdDirective } from './ad.directive';
+import { AdItem } from './ad-item';
+import { AdComponent } from './ad.component';
+
+@Component({
+  selector: 'app-ad-banner',
+  template: `
+    <div class="ad-banner-example">
+      <h3>Advertisements</h3>
+      <ng-template adHost></ng-template>
+    </div>
+  `,
+})
+export class AdBannerComponent implements OnInit, OnDestroy {
+  @Input() ads: AdItem[] = []; // parent calls a service to populate this array, child uses array.
+
+  currentAdIndex = -1;
+
+  @ViewChild(AdDirective, { static: true }) adHost!: AdDirective;
+
+  private clearTimer: VoidFunction | undefined;
+
+  ngOnInit(): void {
+    this.loadComponent(); // grab random ad from ads
+    // this.getAds(); // run loadcomponent every 3 secs.
+  }
+
+  ngOnDestroy() {
+    this.clearTimer?.();
+  }
+
+  loadComponent() {
+    this.currentAdIndex = (this.currentAdIndex + 1) % this.ads.length;
+    const adItem = this.ads[this.currentAdIndex]; // get ad from array
+
+    const viewContainerRef = this.adHost.viewContainerRef; // get container ref
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent<AdComponent>(
+      adItem.component
+    );
+    // create component using container ref
+
+    // insert data
+    componentRef.instance.data = adItem.data;
+  }
+
+  getAds() {
+    const interval = setInterval(() => {
+      this.loadComponent();
+    }, 3000);
+    this.clearTimer = () => clearInterval(interval);
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
